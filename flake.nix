727792ed3a465cbd06d6583d47a5d8959d69ce04
Carlos Salgado <csalgado@uwo.ca>
2024-04-27 11:52:53 -0400

    add llama-rag to template flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
         path = ./templates/langchain-rag;
         description = "Template for a langchain retrieval augmented generation (RAG) project";
       };     
-
+      llama-rag = {
+        path = ./templates/llama-rag;
+        description = "Template for a retrieval augmented generation (RAG) project using LlamaIndex, PyMongo, OpenAI and TruEra's trulens";
+      };    
     };
   };
 }
